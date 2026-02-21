03c872275a3a4ae09814a905c550950f469ce1e6
tsonobe1 <sono.tak.st@gmail.com>
2026-02-21 16:19:49 +0900

    icons 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,23 @@ export default defineNuxtConfig({
     head: {
       title: FALLBACK_SITE_TITLE,
       titleTemplate: `%s | ${FALLBACK_SITE_TITLE}`,
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/favicon.svg',
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+      ],
       meta: [
         {
           name: 'description',
